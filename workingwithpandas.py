185dbda5938dfda868884ef9439110fbe1e2f7a3
unknown <anuragsharmaroy@gmail.com>
2025-05-29 14:09:56 +0530

    modifying panda file

--- workingwithpandas.py
+++ workingwithpandas.py
@@ -1,6 +1,12 @@
-import pandas
-import openpyxl
-excel_path="C:/Users/hp/Desktop/testexcel.xlsx"
-df=pandas.read_excel(excel_path)
-df.head()
-print(df.iloc[0:2,0:2])
+#import pandas
+#import openpyxl
+#excel_path="C:/Users/hp/Desktop/testexcel.xlsx"
+#df=pandas.read_excel(excel_path)
+#df.head()
+#print(df.iloc[0:2,0:2])
+
+import pandas as banana
+
+df=banana.DataFrame({'a':[11,21,31],'b':[21,22,23]})
+
+print(df.head())
